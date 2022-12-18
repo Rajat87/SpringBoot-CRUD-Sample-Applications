2dade8e680a33ca68b9ea3cf0b35219a6f824bf8
Rajat Pandey <rajatpandey090@gmail.com>
2022-12-18 15:55:16 +0530

    Small change made in the single java class created so far

--- NormalCoreJava/src/com/packone/TesterOne.java
+++ NormalCoreJava/src/com/packone/TesterOne.java
@@ -1,10 +1,14 @@
 package com.packone;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TesterOne {
 
 	public static void main(String[] args) {
-		System.out.println("New Line added by Rajat");
-
+		List<Integer>  integers = Arrays.asList(5,6,7,8);
+		System.out.println(integers.stream().map(i-> i*i).collect(Collectors.toList()));
 	}
 
 }
